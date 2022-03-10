6cfd1b0e03f40d7cfeb6e247087c2b9b19ac553e
Marco FORDELONE <72397460+LeThorgrim@users.noreply.github.com>
2022-03-10 11:38:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from urllib import request
 from urllib.error import HTTPError
 
 
-WEBHOOK_URL = 'https://discordapp.com/api/webhooks/xxx/yyy'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/951423564706369626/_dp36dRXPfh-2mD6l5VA0QpsLv_ZlyGSlyZnKhTQsjaHRjWX7nhG_Gss8wVKS_AwDQFk'
 
 # La payload
 payload = {
