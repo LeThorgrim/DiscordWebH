3e83fb8bcc7a32da55e4c3eff4ce7ddec80115a2
Marco FORDELONE <72397460+LeThorgrim@users.noreply.github.com>
2022-03-10 11:54:28 +0100

    Update main.py
    full change

--- main.py
+++ main.py
@@ -1,37 +1,4 @@
-#!/usr/bin/env python3
+from discord_webhook import DiscordWebhook
 
-import json
-from urllib import request
-from urllib.error import HTTPError
-
-
-WEBHOOK_URL = 'https://discord.com/api/webhooks/951423564706369626/_dp36dRXPfh-2mD6l5VA0QpsLv_ZlyGSlyZnKhTQsjaHRjWX7nhG_Gss8wVKS_AwDQFk'
-
-# La payload
-payload = {
-    'content': "Guillaume va ranger ta chambre !"
-}
-
-# Les paramètres d'en-tête de la requête
-headers = {
-    'Content-Type': 'application/json',
-    'user-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'
-}
-
-# Enfin on construit notre requête
-req = request.Request(url=WEBHOOK_URL,
-                      data=json.dumps(payload).encode('utf-8'),
-                      headers=headers,
-                      method='POST')
-
-# Puis on l'émet !
-try:
-    response = request.urlopen(req)
-    print(response.status)
-    print(response.reason)
-    print(response.headers)
-except HTTPError as e:
-    print('ERROR')
-    print(e.reason)
-    print(e.hdrs)
-    print(e.file.read())
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/951423564706369626/_dp36dRXPfh-2mD6l5VA0QpsLv_ZlyGSlyZnKhTQsjaHRjWX7nhG_Gss8wVKS_AwDQFk', content='Webhook Message')
+response = webhook.execute()
